d1cfdd928eb8eb095e11e71ff5a3f7e023fe04e5
JPFrancoia <jeanpatrick.francoia@gmail.com>
2021-11-07 18:29:50 +0000

    Solving day 8 part 2.

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -10,6 +10,9 @@ fn main() {
     let mut code_chars = 0;
     let mut memory_chars = 0;
 
+    //For part 2
+    let mut encoded_chars = 0;
+
     for line in contents.lines() {
         println!("{}", line);
 
@@ -21,14 +24,29 @@ fn main() {
         let count_quote = stripped.matches("\\\"").count();
         let count_hex = re.find_iter(line).count();
 
+        //count_quote -> +2 char per count_quote
+        //count_slash -> +2 chars per count_slash
+        //count_hex -> +1 char per count_hex
+        //+4 for end and start quote
+
+        // For debugging
+        //let tmp_memory_chars = line.len() - count_slash - count_quote - 3 * count_hex - 2;
+        let tmp_encoded_chars = line.len() + 2 * count_quote + 2 * count_slash + count_hex + 4;
+
+        println!("{}\n", tmp_encoded_chars);
+
         code_chars += line.len();
-        let tmp_memory_chars = line.len() - count_slash - count_quote - 3 * count_hex - 2;
         memory_chars += line.len() - count_slash - count_quote - 3 * count_hex - 2;
+        encoded_chars += line.len() + 2 * count_quote + 2 * count_slash + count_hex + 4;
+
 
         //println!("{} {} {} {}", stripped, count_slash, count_quote, count_hex);
-        println!("{} {}\n", line.len(), tmp_memory_chars);
+        //println!("{} {}\n", line.len(), tmp_memory_chars);
     }
 
     println!("Code chars: {}; Memory chars: {}", code_chars, memory_chars);
     println!("Answer: {}", code_chars - memory_chars);
+
+    println!("Code chars: {}; Encoded chars: {}", code_chars, encoded_chars);
+    println!("Answer: {}", encoded_chars - code_chars);
 }
