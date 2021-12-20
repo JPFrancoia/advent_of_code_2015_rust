cf8568f0b91cc17134d254022e2188bebe90a7d3
JPFrancoia <jeanpatrick.francoia@gmail.com>
2021-12-20 20:41:24 +0100

    Solving part 2 of day 12 + cleaning.

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,48 +1,21 @@
-use serde_json::{Result, Value};
-use std::collections::HashMap;
+use serde_json::{Map, Value};
 use std::fs;
 
 fn main() {
     println!("Hello, world!");
 
-    // let contents: HashMap<String, Value> = serde_json::from_str(&fs::read_to_string("input.json").unwrap()).unwrap();
     let contents: Value = serde_json::from_str(&fs::read_to_string("input.json").unwrap()).unwrap();
-
-    let john = serde_json::json!({
-        "name": "John Doe",
-        "age": 43,
-        "phones": [
-            "+44 1234567",
-            "+44 2345678"
-        ],
-        "potatoes": 42,
-        "nested":
-            {"name": "John Doe",
-            "age": 43,
-            "phones": [
-                "+44 1234567",
-                "+44 2345678"
-            ],
-            "potatoes": 42}
-    });
-
-
-    // let john = serde_json::json!([-1,{"a":1}]);
 
     let mut total: i64 = 0;
 
-    // recurse_data(&john["age"]);
-    // recurse_data(&john, &mut total);
     recurse_data(&contents, &mut total);
 
     println!("TOTAL: {}", total);
 }
 
-fn recurse_data(data: &Value, total: &mut i64) -> i64 {
+fn recurse_data(data: &Value, total: &mut i64) {
     match data {
-        Value::String(string) => println!("{}", string),
         Value::Number(nbr) => {
-            println!("{}", nbr);
             *total += nbr.as_i64().unwrap();
         }
         Value::Array(values) => {
@@ -51,12 +24,26 @@ fn recurse_data(data: &Value, total: &mut i64) -> i64 {
             }
         }
         Value::Object(object) => {
-            for (key, value) in object {
+            if !validate_object(object){
+                return;
+            };
+            for value in object.values() {
                 recurse_data(&value, total);
             }
         }
-        _ => println!("other"),
+        _ => (),
     }
+}
 
-    return 0;
+/// Check the immediate values of an object;
+/// reject the object is any immediate value == 'red'
+fn validate_object(object: &Map<String, Value>) -> bool {
+    for value in object.values() {
+        if let Value::String(string) = value {
+            if let "red" = string.as_str() {
+                return false;
+            }
+        }
+    }
+    return true;
 }
